78e832d7b59c2a1e9fe62bc5659461ab5f8d20af
FloraHmmnd <flora.hommand@jellysmack.com>
2025-09-26 13:56:15 +0200

    call data

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from 'vue-router/auto-routes'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import { routes } from 'vue-router/auto-routes';
+import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query';
+
+const app = createApp(App);
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-createApp(App).use(router).mount('#app')
+const queryClient = new QueryClient();
+
+app.use(router);
+app.use(VueQueryPlugin, { queryClient });
+app.mount('#app');
