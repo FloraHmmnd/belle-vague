f2c600f772e4d750500e973a2e7b620c76c652ad
FloraHmmnd <flora.hommand@jellysmack.com>
2025-09-29 11:28:37 +0200

    add g-font

--- src/components/card.vue
+++ src/components/card.vue
@@ -12,7 +12,7 @@ defineProps<{
 
 <template>
   <div
-    class="flex flex-col md:flex-row w-80 max-w-xs bg-neutral rounded-lg shadow-md overflow-hidden"
+    class="flex flex-col md:flex-row w-80 max-w-xs bg-neutral rounded-lg shadow-md overflow-hidden font-quicksand"
   >
     <div class="w-full md:w-40 aspect-[3/2] flex-shrink-0">
       <img
@@ -21,7 +21,7 @@ defineProps<{
         class="w-full h-full object-cover"
       />
     </div>
-    <div class="p-2 flex-1 text-marine">
+    <div class="p-2 flex-1 text-dark">
       <h3 class="font-bold text-lg text-gradient">{{ spot.name }}</h3>
       <p class="text-sm text-sky">{{ spot.address }}</p>
       <div class="mt-1 text-sm">
