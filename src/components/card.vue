6123efd78a539145287b1bae48efe347d3c5b853
FloraHmmnd <89636863+FloraHmmnd@users.noreply.github.com>
2025-10-02 15:47:54 +0200

    add photos (#13)
    Co-authored-by: FloraHmmnd <flora.hommand@jellysmack.com>

--- src/components/card.vue
+++ src/components/card.vue
@@ -19,19 +19,16 @@ const spotPage = computed(() => `/spots/${props.spot.id}`);
     aria-label="See the spot"
     class="flex flex-col lg:flex-row w-80 max-w-xs bg-neutral rounded-lg shadow-md overflow-hidden font-quicksand"
   >
-    <div class="w-full lg:w-40 aspect-[3/2] flex-shrink-0">
-      <img
-        src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=873&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="spot photo"
-        class="w-full h-full object-cover"
-      />
+    <div class="w-full h-60 lg:w-40 aspect-[3/2] flex-shrink-0">
+      <img :src="spot.photo" alt="spot photo" class="w-full h-full object-cover" />
     </div>
     <div class="p-2 flex-1 text-dark">
       <h3 class="font-bold text-lg text-gradient">{{ spot.name }}</h3>
       <p class="text-sm text-sky">{{ spot.address }}</p>
       <div class="mt-1 text-sm">
         <span class="font-semibold text-ultramarine">Level:</span> {{ spot.level }}<br />
-        <span class="font-semibold text-ultramarine">Best season:</span> {{ spot.bestSeason }}<br />
+        <span class="font-semibold text-ultramarine">Best season:</span>
+        {{ spot.bestSeason }}<br />
         <span class="font-semibold text-ultramarine">Water quality:</span>
         {{ spot.waterQuality }}/100<br />
         <span class="font-semibold text-ultramarine">Air temp:</span>
